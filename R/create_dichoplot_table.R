331c40a3a6b4311538a852d21c82b709af99f78b
Catherine Blatter <catherine.blatter@unibas.ch>
2021-02-16 12:40:15 +0100

    update ggBenchmarketing

---
+++ R/create_dichoplot_table.R
@@ -0,0 +1,52 @@
+#' Create a dichotomized table of a selection of variables including the national mean
+#'
+#' @param data a C or E SHURP-dataframe with dichotomized items
+#' @param vars a vector passing variable names
+#' @param restrict defaults to TRUE, passed to shurp2018::prop.yes()
+#' @param restrict_level defaults to TRUE, passed to shurp2018::prop.yes()
+#'
+#' @return
+#' @export
+#'
+#' @examples \dontrun{create_dichoplot_table(shurp2018_C_NA, vars = "C1dich")}
+create_dichoplot_table <- function(data,
+                                   vars,
+                                   restrict = TRUE,
+                                   restrict_level = 10L){
+  if(!is.null(target_center)){
+    center_length <- base::length(target_center)}
+
+  each_center <-
+    data %>%
+    group_by(center) %>%
+    summarise_at(.data, .vars = tidyselect::all_of({{vars}}),
+                 .funs = ~shurp2018::prop.yes(., round = FALSE, restrict = restrict,
+                                              restrict_level = restrict_level)) %>%
+    ungroup() %>%
+    mutate(center = as.character(center),
+           center_label = as.character(center))
+
+
+  national <-
+    data %>%
+    summarise_at(.data, tidyselect::all_of({{vars}}),
+                 .funs = ~shurp2018::prop.yes(., round = FALSE, restrict = restrict, restrict_level = restrict_level)) %>%
+    ungroup() %>%
+    mutate(center = as.character(999),
+           center_label = "SHURP 2018")
+
+
+
+
+  # combine and
+  # create the filling and the color_link for the target center
+  output_data <-
+    bind_rows(each_center, national) %>%
+    mutate(filling_helper = case_when(center == "999" ~ as.character(999),
+                                      TRUE ~ "other")) %>%
+    mutate(fill_values_column = case_when(filling_helper  %in%  "other" ~ "other",
+                                          filling_helper  %in% "999" ~ "national_mean"))
+
+  return(output_data)
+
+}
---
+++ R/ggBenchmarketing.R
@@ -0,0 +1,144 @@
+#' Create the so-called "benchmarketing chart" for overview of percentage agreement
+#'
+#' @param df a dataframe created by create_dichoplot_table()
+#' @param col a variable holding pct-agreement
+#' @param title either a string or a column from label$data
+#' @param text_caption TRUE/FALSE, default TRUE, if caption holding the corresponding
+# item text (to col) should be displayed
+#' @param labs TRUE/FALSE, default TRUE, if Title/subtitle should be displayed
+#' @param language "german" or "french" - default "german", select subtitle and
+# caption
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ggBenchmarketing <- function(df,
+                             col,
+                             title = NULL,
+                             text_caption = TRUE,
+                             labs = TRUE,
+                             language = "german"){
+
+
+  subtitle <-
+    shurp2018::label_data %>%
+    dplyr::filter(var_name == "label_subtitle_benchmarketing") %>%
+    dplyr::pull({{language}})
+
+  # drop nas
+  df <- df %>% dplyr::drop_na({{col}})
+
+  # defining the colours
+  fill_values <- c("other" = "#b2abd2",
+                   "national_mean" = "#5e3c99")
+
+  color_values <- c("national_mean" = "#5e3c99")
+
+  # actual plotting
+  base_plot <-
+    ggplot2::ggplot(df,
+                    ggplot2::aes(x = stats::reorder(center, {{col}}),
+                                 y = {{col}}))+
+    ggplot2::geom_col(ggplot2::aes(fill = fill_values_column)) +
+    ggplot2::geom_hline(aes(yintercept = {{col}}),
+                        data = dplyr::filter(df, center == "999"),
+                        lty = "dashed",
+                        color = "#5e3c99")
+
+
+
+  # #  create
+  # if(pp_diff == FALSE){
+  #   base_plot <-
+  #     base_plot +
+  #     ggrepel::geom_label_repel(aes(colour = fill_values_column,
+  #                                   label = paste0(.data$center_label, ": ", scales::percent({{col}}, .1, 1))),
+  #                               data = filter(df, center %in% c(888, 999)),
+  #                               nudge_x = 0,
+  #                               nudge_y = 1,
+  #                               alpha = 1,
+  #                               label.size = .5,
+  #                               direction = "both",
+  #                               show.legend = F,
+  #                               size = 3.8) }
+  # if(pp_diff == TRUE){
+  #
+  #   subtitle_bb_diff <- if_else(df$ownership_di == "public", label_data$label_public, label_data$label_private)
+  #
+  #   base_plot <-
+  #     base_plot +
+  #     ggrepel::geom_label_repel(aes(colour = fill_values_column,
+  #                                   label = scales::percent({{col}}, .1, 1)),
+  #                               data = filter(df, center %in% c(888, 999)),
+  #                               nudge_x = 0,
+  #                               nudge_y = 1,
+  #                               alpha = 1,
+  #                               label.size = .5,
+  #                               direction = "both",
+  #                               show.legend = F,
+  #                               size = 3.8)}
+
+
+  #add the scales
+  base_plot <-
+    base_plot +
+    ggplot2::scale_x_discrete(breaks = NULL, name = NULL) +
+    ggplot2::scale_y_continuous(name = NULL,
+                                labels = scales::percent_format(accuracy = .1, scale = 1),
+                                breaks = seq(0, 100, 10),
+                                limits = c(0, 100)) +
+    ggplot2::scale_color_manual(values = color_values) +
+    ggplot2::scale_fill_manual(values = fill_values,
+                               name = "% Zustimmende Antworten \nim Vergleich",
+                               labels = c("national_mean" = "Nationaler Durchschnitt",
+                                          "target_center" = "Ihr Pflegeheim",
+                                          "other" = "alle anderen Pflegeheime"))
+
+
+  # adding legends
+  if(labs == TRUE){
+
+    col2 <- stringr::str_extract(deparse(substitute(col)), "\\w{1}\\d{1,2}")
+
+    base_plot <-
+      base_plot +
+      ggplot2::labs(title = paste0(title, ": Item ", col2),
+                    subtitle = {{subtitle}}) }
+
+  # suppress the legends
+  base_plot <-
+    base_plot +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "none",
+                   legend.justification = c(.5, 0),
+                   axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
+                   plot.caption = element_text(size = 10))
+
+  ## add the caption text
+
+
+
+  if(text_caption == TRUE){
+
+    aa <- names(select(df, {{col}}))
+
+    text_data <-
+      if(stringr::str_detect(aa , "C\\d")){shurp2018::QC_text} else {shurp2018::QE_text}
+
+    if(language == "german") {text_data <- filter(text_data, Version == "german")} else {
+
+      text_data <- filter(text_data, Version == "french")}
+
+    caption_text <- text_data %>% rename_with(., .fn = ~paste0(., "dich")) %>%  pull({{col}})
+
+    base_plot <-
+      base_plot +
+      labs(caption = caption_text)
+
+  }
+
+
+  return(base_plot)
+
+}
