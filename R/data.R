c01dd8799d055859aca1a76624da93aea3750822
Catherine Blatter <catherine.blatter@unibas.ch>
2021-02-16 09:44:02 +0100

    added data

--- R/data.R
+++ R/data.R
@@ -5,3 +5,32 @@
 #' @format A data frame with answers from 5311 nurses in  69 variables. More
 #' information is found in the vignette.
 "SHURP_syn"
+
+
+#' QC-Item text data in all languages
+#'
+#' A dataset containing corresponding item texts for QC (including cutoffs for
+# "pct agreement") in all languages
+#'
+#' @format A data frame with 2 rows and 168 variables:
+#' \describe{
+#'   \item{Version}{language, french or german}
+#'   \item{C1}{Text to item C1}
+#'   ...
+#' }
+#' @source SHURP 2018 study
+"QC_data"
+
+#' QE-Item text data in all languages
+#'
+#' A dataset containing corresponding item texts for QE (including cutoffs for
+# "pct agreement") in all languages
+#'
+#' @format A data frame with 2 rows and 91 variables:
+#' \describe{
+#'   \item{Version}{language, french or german}
+#'   \item{E1}{Text to item E1}
+#'   ...
+#' }
+#' @source SHURP 2018 study
+"QE_data"
